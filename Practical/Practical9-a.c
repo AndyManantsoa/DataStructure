ef1dd46306da0025b13a62522d2906e3d5009979
AndyManantsoa <manantsoaadrs@gmail.com>
2024-06-23 18:42:33 +0530

    commit

---
+++ Practical/Practical9-a.c
@@ -0,0 +1,26 @@
+//Bubble sort
+
+#include<stdio.h>
+
+int bubbleSort(){
+
+};
+
+int main(){
+    int n,a;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("\nEnter the elements: ");
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Enter the element that you are searching for: ");
+    scanf("%d", &a);
+
+    
+}
