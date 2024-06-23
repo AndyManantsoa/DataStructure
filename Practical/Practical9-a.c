8b3ef5248f95773e1c825b814f22e8537ebf09e0
AndyManantsoa <manantsoaadrs@gmail.com>
2024-06-23 18:59:47 +0530

    commit

--- Practical/Practical9-a.c
+++ Practical/Practical9-a.c
@@ -2,25 +2,43 @@
 
 #include<stdio.h>
 
-int bubbleSort(){
+void swap(int *a,int *b){
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
 
-};
+int bubbleSort(int arr[],int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+            }
+        }
+    }
+}
+
+void printArr(int arr[],int size){
+    printf("Your array: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
 
 int main(){
-    int n,a;
-
+    int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
     int arr[n];
-
     printf("\nEnter the elements: ");
-
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-
-    printf("Enter the element that you are searching for: ");
-    scanf("%d", &a);
-
+    printf("\nYour initial array: ");
+    printArr(arr,n);
     
+    bubbleSort(arr,n);
+    printf("\nYour sorted array: ");
+    printArr(arr,n);
+
 }
