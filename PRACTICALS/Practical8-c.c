3ab0fe973b2f336318ea97300fcef6fff365fe11
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-04 11:26:03 +0530

    commit

--- PRACTICALS/Practical8-c.c
+++ PRACTICALS/Practical8-c.c
@@ -2,11 +2,34 @@
 
 #include<stdio.h>
 
+void insertionSort(int arr[], int size){
+    int j, key;
+
+    for(int i = 1; i < size; i++){
+        key = arr[i];
+        j = i - 1; 
+
+        while(j >= 0 && key < arr[j]){ 
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArr(int arr[],int size){
+    printf("Your array: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
 int interpolationSearch(int arr[],int left,int right,int key){
     int prob = left + (key-arr[left])*(right-left)/(arr[right]-arr[left]);
     printf("Element found at index %d",prob);
     return prob;
 }
+
 
 int main(){
 
@@ -23,6 +46,11 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
+
+    insertionSort(arr,n);
+    printf("Sorted array: ");
+
+    printArr(arr,n);
 
     printf("Enter the element that you are searching for: ");
     scanf("%d", &a);
