3e2af238d31b6e9d37067357a2b6044a81a197ec
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-04 10:03:20 +0530

    commit

--- PRACTICALS/Practical9-e.c
+++ PRACTICALS/Practical9-e.c
@@ -2,19 +2,62 @@
 
 #include<stdio.h>
 
-void swap(int *a,int *b){
-    int temp = *a;
-    *a=*b;
-    *b=temp;
+void merge(int arr[], int left, int middle, int right) {
+    int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[middle + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void merge(){
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
 
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        merge(arr, left, middle, right);
+    }
 }
 
-void mergeSort(int arr[],int size,int left, int right){
-    
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
+
 
 void printArr(int arr[],int size){
     printf("Your array: ");
