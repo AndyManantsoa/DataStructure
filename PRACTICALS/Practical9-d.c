5b43ac5729bfb28379530e86b62c2f4fa068f8e3
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-09 12:48:01 +0530

    commit

--- PRACTICALS/Practical9-d.c
+++ PRACTICALS/Practical9-d.c
@@ -7,9 +7,32 @@ void swap(int *a,int *b){
     *a=*b;
     *b=temp;
 }
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
 
-int quickSort(int arr[],int size){
-    
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high) {
+
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
 void printArr(int arr[],int size){
@@ -34,7 +57,7 @@ int main(){
     printf("\nYour initial array: ");
     printArr(arr,n);
     
-    quickSort(arr,n);
+    quickSort(arr,0,n);
 
     printf("\nYour sorted array: ");
     printArr(arr,n);
