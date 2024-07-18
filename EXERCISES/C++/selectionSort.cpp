e19e03a99b01a14c7a6cf158d138713dcc5d8cc7
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-18 16:09:33 +0530

    commit

---
+++ EXERCISES/C++/selectionSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int index=i;
+        for(int j=0;j<n-1;j++){
+            if(arr[j]<);
+        }
+    }
+};
+
+int main(){
+    int n;
+    std::cout<<"Enter the size of the array: ";
+    std::cin>>n;
+    int arr[n];
+    std::cout<<"Enter the elements of the array: ";
+
+    for(int i =0;i<n;i++){
+        std::cin>>arr[i];
+    }
+    int arr[n];
+    
+    selectionSort(arr,n);
+}
