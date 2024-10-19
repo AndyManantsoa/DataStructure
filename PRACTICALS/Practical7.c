c1c39aae780c7d15218faa2a0def0dbbc5fe2910
andymanantsoa <manantsoaadrs@gmail.com>
2024-10-19 20:34:48 +0530

    Updates

--- PRACTICALS/Practical7.c
+++ PRACTICALS/Practical7.c
@@ -1,30 +1,139 @@
 //7. Implement Double Linked lists and its operations(creation insertion deletion traversal search reverse)
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
+// Define the structure for a node in the doubly linked list
+struct Node {
     int data;
-    struct Node* next;
     struct Node* prev;
-}Node;
+    struct Node* next;
+};
 
-Node* createNode(int data){
-    Node* newNode = (Node*)malloc(sizeof(Node));
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
-    newNode->next = NULL;
     newNode->prev = NULL;
+    newNode->next = NULL;
     return newNode;
 }
 
-void insert(Node ** Head , int data, int position){
-    Node* newNode = createNode(data);
-    if (position ==1){
-        newNode->next = *Head;
-        if (*Head != NULL){
-            (*Head)->prev = newNode;
-        }
-        *Head = newNode;
+// Function to insert a node at the beginning of the list
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    if (*head != NULL) {
+        (*head)->prev = newNode;
     }
+    *head = newNode;
+}
+
+// Function to insert a node at the end of the list
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+// Function to delete a node with a given key
+void deleteNode(struct Node** head, int key) {
+    if (*head == NULL) return;
+
+    struct Node* temp = *head;
     
+    // If the head node itself holds the key to be deleted
+    if (temp->data == key) {
+        *head = temp->next;
+        if (*head != NULL) {
+            (*head)->prev = NULL;
+        }
+        free(temp);
+        return;
+    }
+
+    // Search for the key to be deleted
+    while (temp != NULL && temp->data != key) {
+        temp = temp->next;
+    }
+
+    // If key was not present in linked list
+    if (temp == NULL) return;
+
+    // Unlink the node from linked list
+    temp->prev->next = temp->next;
+    if (temp->next != NULL) {
+        temp->next->prev = temp->prev;
+    }
+
+    free(temp);
+}
+
+// Function to traverse and print the list
+void traverseList(struct Node* head) {
+    struct Node* temp = head;
+    printf("List: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Function to search for a key in the list
+struct Node* searchNode(struct Node* head, int key) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        if (temp->data == key) {
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+// Function to reverse the list
+void reverseList(struct Node** head) {
+    struct Node* temp = NULL;
+    struct Node* current = *head;
+
+    while (current != NULL) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+
+    if (temp != NULL) {
+        *head = temp->prev;
+    }
+}
+
+// Main function to test the doubly linked list operations
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtEnd(&head, 3);
+    insertAtBeginning(&head, 0);
+
+    printf("After insertion: ");
+    traverseList(head);
+
+    deleteNode(&head, 2);
+    printf("After deleting 2: ");
+    traverseList(head);
+
+    int searchKey = 3;
+    struct Node* searchResult = searchNode(head, searchKey);
+    return 0;
 }
