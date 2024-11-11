0e8dbcc1f1c9c50f4708b1ac01ecb59c4a205cd8
andymanantsoa <manantsoaadrs@gmail.com>
2024-11-11 23:43:31 +0530

    update

--- PRACTICALS/Practical4-d.c
+++ PRACTICALS/Practical4-d.c
@@ -1,31 +1,27 @@
-//Tower of hanoi using recursion
+// Tower of Hanoi using recursion
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 
-
-void towerOfHanoi(int n,char Src, char Aux, char Dest){
-    if(n==0){
-        printf("cannot be 0\n");
+void towerOfHanoi(int n, char Src, char Aux, char Dest) {
+    if (n == 0) {
+        printf("Cannot be 0\n");
         return;
     }
-    if(n==1){
-        printf("Disk 1 moved from %c to %c",Src,DeS)
-        return;
+    if (n == 1) {
+        printf("Disk 1 moved from %c to %c\n", Src, Dest);
     }
 
-    towerOfHanoi(n-1,Src,Dest,Aux);
-    printf("Disk %d moved from %c to %c",n,Src,Dest);
-    towerOfHanoi(n-1,Aux,Source,Dest);
-
+    towerOfHanoi(n - 1, Src, Dest, Aux);
+    printf("Disk %d moved from %c to %c\n", n, Src, Dest);
+    towerOfHanoi(n - 1, Aux, Src, Dest);
 }
 
-int main(){
+int main() {
     int n;
-    printf("Enter the number of disk: ");
-    scanf("%d",%n);
-    
-    towerOfHanoi(n,'A','B','C');
-    
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+
+    towerOfHanoi(n, 'A', 'B', 'C');
+
     return 0;
 }
